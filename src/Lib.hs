739d0e43503d1947fa5efd25188984f2d0cf60a6
Davide Giovannini <giovannini.davide90@gmail.com>
2016-12-08 05:27:52 +0100

    Improved repl
    Added quit functionality, improved prompt and value and error display

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,19 +1,32 @@
 module Lib
     ( replFunc
-    ) where
+    )
+where
+
+import System.IO
 
 import Parsing(readExpr)
 import Evaluation(eval)
 
-import Control.Monad(forever)
+import Control.Monad(unless)
 import Control.Monad.Except
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: IO String
+readPrompt = flushStr "λ:" >> getLine
+
+readAndEval :: String -> IO ()
+readAndEval expr =
+  case runExcept replAction of
+    Right val -> flushStr "> " >> print val
+    Left val  -> flushStr "! " >> print val
+  where replAction = readExpr expr >>= eval
+
+
 replFunc :: IO ()
-replFunc = forever $ do
-    putStrLn "λ:"
-    input <- getLine
-    let parsed = readExpr input
-    let evalued = parsed >>= eval
-    case runExcept evalued of
-      Right right -> print right
-      Left left   -> print left
+replFunc = do
+  input <- readPrompt
+  unless (input == "quit") $
+    readAndEval input >> replFunc
